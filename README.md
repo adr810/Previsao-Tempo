ee82df5c72fafa3a64658782dbd56ae01908c9d4
adr810 <adr.flp810@gmail.com>
2022-09-10 06:24:53 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+aplicativo de previsão do tempo utilizando React e essas API'S:
+💻 OpenWeather API: https://openweathermap.org/
+💻 GeoDB Cities API: https://rapidapi.com/wirefreethought/api/geodb-cities/
+
+para instalação e utilização use o npm install e npm start
---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "1c93205f9fmshbfd4cfa3a356035p14819ejsnbc8ac96f9cb4",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "2250337a4352c9365ca655d955ab577d";
+export const WEATHER_LANG = "pt_br";
